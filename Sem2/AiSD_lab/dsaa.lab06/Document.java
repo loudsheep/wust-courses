c6ca3809a404d7599db198992f51d9258d4daa89
loudsheep <loudsheep05@gmail.com>
2025-04-10 12:40:32 +0200

    aisd: lab06, merge and radix sort

--- Sem2/AiSD_lab/dsaa.lab06/Document.java
+++ Sem2/AiSD_lab/dsaa.lab06/Document.java
@@ -1,85 +1,282 @@
 package dsaa.lab06;
 
-import java.util.ListIterator;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-public class Document{
-	public String name;
-	public TwoWayCycledOrderedListWithSentinel<Link> link;
-	public Document(String name, Scanner scan) {
-		this.name=name.toLowerCase();
-		link=new TwoWayCycledOrderedListWithSentinel<Link>();
-		load(scan);
-	}
-	public void load(Scanner scan) {
-		//TODO
-	}
-	
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	
+public class Document {
+    public String name;
+    public TwoWayCycledOrderedListWithSentinel<Link> link;
 
-	public static boolean isCorrectId(String id) {
-		//TODO
-		return false;
-	}
+    public Document(String name, Scanner scan) {
+        this.name = name.toLowerCase();
+        link = new TwoWayCycledOrderedListWithSentinel<>();
+        load(scan);
+    }
 
-	// accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
-	public static Link createLink(String link) {
-		//TODO
-		return null;
-	}
+    public void load(Scanner scan) {
+        while (true) {
+            String line = scan.nextLine();
 
-	@Override
-	public String toString() {
-		String retStr="Document: "+name;
-		//TODO
-		return retStr;
-	}
+            if (Objects.equals(line, "eod")) break;
 
-	public String toStringReverse() {
-		String retStr="Document: "+name;
-		ListIterator<Link> iter=link.listIterator();
-		while(iter.hasNext())
-			iter.next();
-		//TODO
-		while(iter.hasPrevious()){
-		}
-		return retStr;
-	}
-	public int[] getWeights() {
-		//TODO
-		return null;
-	}
+            String[] words = line.split("\\s+");
 
-	public static void showArray(int[] arr) {
-		// TODO
-	}
+            for (String word : words) {
+                if (correctLink(word)) {
+                    String[] split = word.split("=");
+                    Link tmp = createLink(split[1]);
 
-	void bubbleSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
+                    if (tmp != null) {
+                        link.add(tmp);
+                    }
+                }
+            }
+        }
+    }
 
-	public void insertSort(int[] arr) {
-		showArray(arr);
-		//TODO
-		
-	}
-	public void selectSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
-	
+    public static boolean correctLink(String link) {
+        Pattern pattern = Pattern.compile("^[Ll][Ii][Nn][Kk]=[a-zA-Z][a-zA-Z0-9_]*([(]\\d+[)])?$");
+        Matcher matcher = pattern.matcher(link);
 
-	public void iterativeMergeSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
+        return matcher.matches();
+    }
 
-	public void radixSort(int[] arr) {
-		showArray(arr);
-		//TODO
-	}
+    public static boolean isCorrectId(String id) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z][a-zA-Z0-9_]*$");
+        Matcher matcher = pattern.matcher(id);
+
+        return matcher.matches();
+    }
+
+    // accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
+    public static Link createLink(String link) {
+        Pattern pattern = Pattern.compile("^([a-zA-Z][a-zA-Z0-9_]*)([(]\\d+[)])?$");
+        Matcher matcher = pattern.matcher(link);
+
+        if (!matcher.matches()) return null;
+
+        String[] firstSplit = link.split("[(]");
+        String ref = firstSplit[0].toLowerCase();
+        if (firstSplit.length > 1) {
+            String[] secondSplit = firstSplit[1].split("[)]");
+            int weight = Integer.parseInt(secondSplit[0]);
+
+            if (weight <= 0) return null;
+
+            return new Link(ref, weight);
+        }
+        return new Link(ref);
+    }
+
+    @Override
+    public String toString() {
+        String retStr = "Document: " + name;
+        Iterator<Link> iter = link.iterator();
+        int i = 0;
+        while (iter.hasNext() && i < 10) {
+            if (i == 0) retStr += "\n";
+            else retStr += " ";
+
+            retStr += iter.next();
+            i++;
+        }
+
+        i = 0;
+        while (iter.hasNext()) {
+            if (i == 0) retStr += "\n";
+            else retStr += " ";
+
+            retStr += iter.next();
+            i++;
+        }
+
+        return retStr;
+    }
+
+    public String toStringReverse() {
+        ListIterator<Link> iter = link.listIterator();
+        while (iter.hasNext())
+            iter.next();
 
 
+        String retStr = "Document: " + name;
+        int i = 0;
+        while (iter.hasPrevious() && i < 10) {
+            if (i == 0) retStr += "\n";
+            else retStr += " ";
+
+            retStr += iter.previous();
+            i++;
+        }
+
+        i = 0;
+        while (iter.hasPrevious()) {
+            if (i == 0) retStr += "\n";
+            else retStr += " ";
+
+            retStr += iter.previous();
+            i++;
+        }
+
+        return retStr;
+    }
+
+    public int[] getWeights() {
+        int[] weights = new int[link.size()];
+
+        Iterator<Link> iter = link.iterator();
+        int i = 0;
+        while (iter.hasNext()) {
+            weights[i] = iter.next().weight;
+            i++;
+        }
+
+        return weights;
+    }
+
+    public static void showArray(int[] arr) {
+        String res = Arrays.stream(arr)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(res);
+    }
+
+    private static void swapElements(int[] arr, int l, int r) {
+        int tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
+    }
+
+    void bubbleSort(int[] arr) {
+        showArray(arr);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swapElements(arr, j, j - 1);
+                }
+            }
+            showArray(arr);
+        }
+    }
+
+    public void insertSort(int[] arr) {
+        showArray(arr);
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            int value = arr[i];
+            int tmp;
+            int j = i;
+            for (; j < arr.length - 1; j++) {
+                tmp = arr[j + 1];
+                if (value < arr[j + 1]) break;
+
+                arr[j] = tmp;
+            }
+
+            arr[j] = value;
+            showArray(arr);
+        }
+    }
+
+    public void selectSort(int[] arr) {
+        showArray(arr);
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            int maxPos = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[maxPos]) {
+                    maxPos = j;
+                }
+            }
+
+            swapElements(arr, i, maxPos);
+            showArray(arr);
+        }
+    }
+
+    private static void merge(int[] array, int[] tmp, int left, int mid, int right) {
+        int i = left;
+        int j = mid;
+        int k = left;
+
+        while (i < mid && j < right) {
+            if (array[i] <= array[j]) {
+                tmp[k++] = array[i++];
+            } else {
+                tmp[k++] = array[j++];
+            }
+        }
+
+        while (i < mid) {
+            tmp[k++] = array[i++];
+        }
+
+        while (j < right) {
+            tmp[k++] = array[j++];
+        }
+    }
+
+    public void iterativeMergeSort(int[] arr) {
+        showArray(arr);
+
+        int n = arr.length;
+        int[] tmp = new int[n];
+
+        for (int size = 1; size < n; size *= 2) {
+            for (int leftStart = 0; leftStart < n; leftStart += 2 * size) {
+                int mid = Math.min(leftStart + size, n);
+                int rightEnd = Math.min(leftStart + 2 * size, n);
+
+                merge(arr, tmp, leftStart, mid, rightEnd);
+            }
+
+            System.arraycopy(tmp, 0, arr, 0, n);
+            showArray(arr);
+        }
+    }
+
+    private void countingSortByDigit(int[] array, int exp) {
+        int n = array.length;
+        int[] output = new int[n];
+        int[] count = new int[10]; // dla cyfr 0-9
+
+        // Zliczanie wystąpień cyfr w pozycji exp
+        for (int i = 0; i < n; i++) {
+            int digit = (array[i] / exp) % 10;
+            count[digit]++;
+        }
+
+        count[0]--;
+        // Przekształcenie count[i] w pozycje końcowe w output
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Budowanie tablicy wyjściowej (od końca dla stabilności)
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = (array[i] / exp) % 10;
+            output[count[digit]] = array[i];
+            count[digit]--;
+        }
+
+        // Skopiowanie posortowanej tablicy do oryginału
+        System.arraycopy(output, 0, array, 0, n);
+    }
+
+    public void radixSort(int[] array) {
+        showArray(array);
+
+        int max = 999;
+        int exp = 1;
+
+        while (max / exp > 0) {
+            countingSortByDigit(array, exp);
+            showArray(array);
+            exp *= 10;
+        }
+    }
 }
--- Sem2/AiSD_lab/dsaa.lab06/TwoWayCycledOrderedListWithSentinel.java
+++ Sem2/AiSD_lab/dsaa.lab06/TwoWayCycledOrderedListWithSentinel.java
@@ -2,197 +2,313 @@ package dsaa.lab06;
 
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class TwoWayCycledOrderedListWithSentinel<E extends Comparable<E>> implements IList<E> {
+
+    private class Element {
+        E value;
+        Element next;
+        Element prev;
 
 
-public class TwoWayCycledOrderedListWithSentinel<E> implements IList<E>{
+        public Element(E e) {
+            this(e, null, null);
+        }
 
-	private class Element{
-		public Element(E e) {
-			//TODO
-		}
-		public Element(E e, Element next, Element prev) {
-			//TODO
-		}
-		// add element e after this
-		public void addAfter(Element elem) {
-			//TODO
-		}
-		// assert it is NOT a sentinel
-		public void remove() {
-			//TODO
-		}
-		E object;
-		Element next=null;
-		Element prev=null;
-	}
+        public Element(E e, Element next, Element prev) {
+            this.value = e;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        // add element e after this
+        public void addAfter(Element elem) {
+            elem.next = this.next;
+            this.next.prev = elem;
+            this.next = elem;
+            elem.prev = this;
+        }
+
+        public void addBefore(Element elem) {
+            elem.prev = this.prev;
+            this.prev.next = elem;
+            this.prev = elem;
+            elem.next = this;
+        }
+
+        // assert it is NOT a sentinel
+        public void remove() {
+            assert this != sentinel;
+
+            if (next != null) {
+                this.next.prev = this.prev;
+            }
+            if (prev != null) {
+                this.prev.next = this.next;
+            }
+        }
+    }
+
+    Element sentinel;
+    int size;
+
+    private class InnerIterator implements Iterator<E> {
+        Element current;
+
+        public InnerIterator() {
+            this.current = sentinel;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current.next != null && current.next != sentinel;
+        }
+
+        @Override
+        public E next() {
+            current = current.next;
+            return current.value;
+        }
+    }
+
+    private class InnerListIterator implements ListIterator<E> {
+        Element current;
+
+        public InnerListIterator() {
+            current = sentinel;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current.next != null && current.next != sentinel;
+        }
+
+        @Override
+        public E next() {
+            current = current.next;
+            return current.value;
+        }
+
+        @Override
+        public void add(E arg0) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return current.prev != null && current != sentinel;
+        }
+
+        @Override
+        public int nextIndex() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public E previous() {
+            E toRet = current.value;
+            current = current.prev;
+            return toRet;
+        }
+
+        @Override
+        public int previousIndex() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void set(E arg0) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public TwoWayCycledOrderedListWithSentinel() {
+        this.clear();
+    }
+
+    //@SuppressWarnings("unchecked")
+    @Override
+    public boolean add(E e) {
+        if (size == 0) {
+            this.sentinel.addAfter(new Element(e));
+            size++;
+            return true;
+        }
+
+        Element tmp = sentinel;
+        while (tmp.next != null && tmp.next != sentinel) {
+            if (tmp.next.value.compareTo(e) > 0) {
+                break;
+            }
+            tmp = tmp.next;
+        }
+
+        tmp.addAfter(new Element(e));
+        size++;
+
+        return true;
+    }
+
+    private Element getElement(int index) {
+        if (index < 0 || index >= size) throw new NoSuchElementException();
+
+        Element tmp = sentinel.next;
+        while (index > 0 && tmp != null && tmp != this.sentinel) {
+            index--;
+            tmp = tmp.next;
+        }
+
+        if (index > 0 || tmp == null) throw new NoSuchElementException();
+
+        return tmp;
+    }
+
+    private Element getElement(E obj) {
+        Element tmp = sentinel.next;
+        while (tmp.next != null && tmp != this.sentinel) {
+            if (tmp.value.equals(obj)) return tmp;
+            tmp = tmp.next;
+        }
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        this.sentinel = new Element(null);
+        this.sentinel.next = this.sentinel;
+        this.sentinel.prev = this.sentinel;
+        this.size = 0;
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return this.indexOf(element) >= 0;
+    }
+
+    @Override
+    public E get(int index) {
+        return this.getElement(index).value;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(E element) {
+        Element tmp = sentinel.next;
+        int pos = 0;
+        while (tmp != null && tmp != sentinel) {
+            if (tmp.value.equals(element)) return pos;
+            tmp = tmp.next;
+            pos++;
+        }
+
+        return -1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size <= 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new InnerIterator();
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return new InnerListIterator();
+    }
+
+    @Override
+    public E remove(int index) {
+        Element tmp = getElement(index);
+        tmp.remove();
+        size--;
+        return tmp.value;
+    }
+
+    @Override
+    public boolean remove(E e) {
+        Element tmp = getElement(e);
+        if (tmp == null) return false;
+
+        tmp.remove();
+        size--;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //@SuppressWarnings("unchecked")
+    public void add(TwoWayCycledOrderedListWithSentinel<E> other) {
+        if (other == this || other.isEmpty()) return;
+        if (isEmpty()) {
+            this.sentinel = other.sentinel;
+            this.size = other.size;
+            other.clear();
+            return;
+        }
 
 
-	Element sentinel;
-	int size;
+        Element thisElem = sentinel.next;
+        Element otherElem = other.sentinel.next;
 
-	private class InnerIterator implements Iterator<E>{
-		//TODO
-		public InnerIterator() {
-			//TODO
-		}
-		@Override
-		public boolean hasNext() {
-			//TODO
-			return false;
-		}
+        while (thisElem != null && thisElem != this.sentinel && otherElem != null && otherElem != other.sentinel) {
+            // if this <= other -> skip this
+            // if this > other -> insert other before this, skip this, skip other
+            if (thisElem.value.compareTo(otherElem.value) <= 0) {
+                thisElem = thisElem.next;
+            } else {
+                Element next = otherElem.next;
+                thisElem.addBefore(otherElem);
+                otherElem = next;
+            }
+        }
 
-		@Override
-		public E next() {
-			//TODO
-			return null;
-		}
-	}
+        thisElem = thisElem.prev;
+        while (otherElem != null && otherElem != other.sentinel) {
+            Element next = otherElem.next;
+            thisElem.addAfter(otherElem);
+            thisElem = thisElem.next;
+            otherElem = next;
+        }
 
-	private class InnerListIterator implements ListIterator<E>{
-		//TODO
-		public InnerListIterator() {
-			//TODO
-		}
-		@Override
-		public boolean hasNext() {
-			//TODO
-			return false;
-		}
+        this.size += other.size;
+        other.clear();
+    }
 
-		@Override
-		public E next() {
-			//TODO
-			return null;
-		}
-		@Override
-		public void add(E arg0) {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public boolean hasPrevious() {
-			//TODO
-			return false;
-		}
-		@Override
-		public int nextIndex() {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public E previous() {
-			//TODO
-			return null;
-		}
-		@Override
-		public int previousIndex() {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public void set(E arg0) {
-			throw new UnsupportedOperationException();
-		}
-	}
-	public TwoWayCycledOrderedListWithSentinel() {
-		//TODO
-	}
+    //@SuppressWarnings({ "unchecked", "rawtypes" })
+    public void removeAll(E e) {
+        Element tmp = sentinel.next;
 
-	//@SuppressWarnings("unchecked")
-	@Override
-	public boolean add(E e) {
-		//TODO
-		return false;
-	}
-
-	private Element getElement(int index) {
-		//TODO
-		return null;
-	}
-
-	private Element getElement(E obj) {
-		//TODO
-		return null;
-	}
-
-	@Override
-	public void add(int index, E element) {
-		throw new UnsupportedOperationException();
-
-	}
-
-	@Override
-	public void clear() {
-		//TODO
-	}
-
-	@Override
-	public boolean contains(E element) {
-		//TODO
-		return false;
-	}
-
-	@Override
-	public E get(int index) {
-		//TODO
-		return null;
-	}
-
-	@Override
-	public E set(int index, E element) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public int indexOf(E element) {
-		//TODO
-		return -1;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		//TODO
-		return true;
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		return new InnerIterator();
-	}
-
-	@Override
-	public ListIterator<E> listIterator() {
-		return new InnerListIterator();
-	}
-
-	@Override
-	public E remove(int index) {
-		//TODO
-		return null;
-	}
-
-	@Override
-	public boolean remove(E e) {
-		//TODO
-		return false;
-	}
-
-	@Override
-	public int size() {
-		//TODO
-		return -1;
-	}
-
-	//@SuppressWarnings("unchecked")
-	public void add(TwoWayCycledOrderedListWithSentinel<E> other) {
-		//TODO
-	}
-	
-	//@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void removeAll(E e) {
-		//TODO
-	}
-
+        boolean deleted = false;
+        while (tmp != null && tmp != sentinel) {
+            if (tmp.value.equals(e)) {
+                tmp.remove();
+                size--;
+                deleted = true;
+            } else if (deleted) {
+                break;
+            }
+            tmp = tmp.next;
+        }
+    }
 }
-
