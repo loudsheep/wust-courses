3606a73995854029f84b55809dac007375ac552f
loudsheep <loudsheep05@gmail.com>
2025-04-23 10:56:14 +0200

    aisd: slight improvements in time

--- Sem2/AiSD/r1d_dzielo_bajtycha_py/dfs1.py
+++ Sem2/AiSD/r1d_dzielo_bajtycha_py/dfs1.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.setrecursionlimit(1_000_000)
 
 n, k = map(int, input().split())
@@ -9,37 +10,39 @@ for i in range(2, n + 1):
     parent = int(input())
     tree[parent].append(i)
 
-dp = [ [float('-inf')] * (k + 1) for _ in range(n + 1) ]    # wybrany z korzeniem
-ndp = [ [float('-inf')] * (k + 1) for _ in range(n + 1) ]   # bez
+dp = [[float("-inf")] * (k + 1) for _ in range(n + 1)]  # wybrany z korzeniem
+ndp = [[float("-inf")] * (k + 1) for _ in range(n + 1)]  # bez
+
 
 def dfs(node):
     dp[node][1] = values[node]  # wybieramy tylko ten wierzchołek
-    ndp[node][0] = 0            # nie wybieramy aktualnego
+    ndp[node][0] = 0  # nie wybieramy aktualnego
 
     for child in tree[node]:
         dfs(child)
 
         # tmpp tablice na wyniki
-        new_dp = [float('-inf')] * (k + 1)
-        new_ndp = [float('-inf')] * (k + 1)
+        new_dp = [float("-inf")] * (k + 1)
+        new_ndp = [float("-inf")] * (k + 1)
 
         for i in range(k + 1):
-            if ndp[node][i] == float('-inf'):
+            if ndp[node][i] == float("-inf"):
                 continue
             for j in range(k + 1 - i):
                 best = max(dp[child][j], ndp[child][j])
-                if best != float('-inf'):
+                if best != float("-inf"):
                     new_ndp[i + j] = max(new_ndp[i + j], ndp[node][i] + best)
 
         for i in range(k + 1):
-            if dp[node][i] == float('-inf'):
+            if dp[node][i] == float("-inf"):
                 continue
             new_dp[i] = dp[node][i]
 
         dp[node] = new_dp
         ndp[node] = new_ndp
 
+
 dfs(1)
 
 result = max(dp[1][k], ndp[1][k])
-print(-1 if result == float('-inf') else result)
+print(-1 if result == float("-inf") else result)
--- Sem2/AiSD/r1d_dzielo_bajtycha_py/dfs2.py
+++ Sem2/AiSD/r1d_dzielo_bajtycha_py/dfs2.py
@@ -1,11 +1,41 @@
-######################### Wejście
+import sys
+
+sys.setrecursionlimit(1_000_000)
+
 n, k = map(int, input().split())
-values = [0] + list(map(int, input().split()))  # indeksujemy od 1
+values = [0] + list(map(int, input().split()))
 
 tree = [[] for _ in range(n + 1)]
 for i in range(2, n + 1):
     parent = int(input())
     tree[parent].append(i)
-    
-######################### Obliczenia
 
+dp = [[float("-inf")] * (k + 1) for _ in range(n + 1)]  # node selected
+ndp = [[float("-inf")] * (k + 1) for _ in range(n + 1)]  # node not selected
+
+
+def dfs(node):
+    dp[node][1] = values[node]
+    ndp[node][0] = 0
+
+    for child in tree[node]:
+        dfs(child)
+
+        best = [max(dp[child][j], ndp[child][j]) for j in range(k + 1)]
+
+        new_ndp = [float("-inf")] * (k + 1)
+        for i in range(k + 1):
+            if ndp[node][i] == float("-inf"):
+                continue
+            for j in range(k + 1 - i):
+                if best[j] != float("-inf"):
+                    new_ndp[i + j] = max(new_ndp[i + j], ndp[node][i] + best[j])
+
+        ndp[node] = new_ndp
+        # dp[node] is unchanged (children can't be used)
+
+
+dfs(1)
+
+result = max(dp[1][k], ndp[1][k])
+print(-1 if result == float("-inf") else result)
