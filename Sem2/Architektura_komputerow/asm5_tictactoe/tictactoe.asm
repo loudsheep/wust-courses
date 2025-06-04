0f4c3caf0109fce21bb4139a9c9a092d20652055
loudsheep <loudsheep05@gmail.com>
2025-06-04 08:47:59 +0200

    ak: finished tictactoe

--- Sem2/Architektura_komputerow/asm5_tictactoe/tictactoe.asm
+++ Sem2/Architektura_komputerow/asm5_tictactoe/tictactoe.asm
@@ -9,19 +9,19 @@
 	
 	# Prompty
 	prompt1: .asciiz "Podaj liczbe rund do rozegrania"
-	blednyInput: .asciiz "Wprowadzono bledne dane\n"
+	badInput: .asciiz "Wprowadzono bledne dane\n"
 	prompt3: .asciiz "Twoj ruch! Postaw X w wybranym miejscu wpisujac numer dostepnego pola"
 	round: .asciiz "RUNDA NR "
 	prompt4: .asciiz "Runde wygrywa "
 	prompt5: .asciiz "Aktualny wynik"
 	prompt6: .asciiz "KONIEC GRY! ZWYCIEZCA PARTII ZOSTAJE "
-	prompt7: .asciiz "Remis! Obaj gracze otrzymuja punkt"
+	prompt7: .asciiz "Tie! Obaj gracze otrzymuja punkt"
 	prompt8: .asciiz "Bledny ruch! Wybierz inne pole"
 	prompt10: .asciiz "KONIEC GRY! PARTIA ZAKONCZONA REMISEM"
 	prompt11: .asciiz "Komputer wykonal swoj ruch!"
 	
 	# Inne
-	plansza: .space 9
+	board: .space 9
 .text
 	# Ogolne
 	main:
@@ -29,33 +29,33 @@
 		move $s2, $zero
 		move $s3, $zero
 		
-		j liczbaRund
+		j number_of_rounds
 		
-	wroc:
+	back:
     		jr $ra
     		
     	exit:
     		li $v0, 10
     		syscall
     		
-	blednyInput_link:
+	badInput_link:
     		li $v0, 4
-    		la $a0, blednyInput
+    		la $a0, badInput
     		syscall
-    		j wroc
+    		j back
     		
-    	# Plansza
-    	wyswietlPlansze_link:
+    	# Board
+    	showBoard_link:
     		move $t0, $zero
     		
     		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal petlaWyswietlPlansze_link
+		jal showBoardLoop_link
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
 		
-    		j wroc
-    	petlaWyswietlPlansze_link:
+    		j back
+    	showBoardLoop_link:
     		move $t1, $zero
     		
     		li $v0, 11
@@ -64,7 +64,7 @@
     		
     		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal petlaWyswietlPlanszeRzad_link
+		jal showBoardRowLoop_link
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
 		
@@ -74,7 +74,7 @@
     		lb $a0, newLine
     		syscall
     		
-    		beq $t0, 3, wroc
+    		beq $t0, 3, back
     		
     		li $v0, 4
     		la $a0, floor
@@ -82,46 +82,48 @@
     		li $v0, 11
     		lb $a0, newLine
     		syscall
-    		j petlaWyswietlPlansze_link
-    	petlaWyswietlPlanszeRzad_link:
+    		j showBoardLoop_link
+
+    	showBoardRowLoop_link:
     		mul $t2, $t0, 3
     		add $t2, $t2, $t1
     		li $v0, 11
-    		lb $a0, plansza($t2)
+    		lb $a0, board($t2)
     		syscall
     		
     		addi $t1, $t1, 1
-    		beq $t1, 3, wroc
+    		beq $t1, 3, back
     		lb $a0, space
     		syscall
     		lb $a0, wall
     		syscall
     		lb $a0, space
     		syscall
-    		j petlaWyswietlPlanszeRzad_link
-	resetPlansza_link:
+    		j showBoardRowLoop_link
+
+	resetBoard_link:
 		addi $t0, $zero, 1
-		la $t1, plansza
+		la $t1, board
 		
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal petlaResetPlansza_link
+		jal resetBoardLoop_link
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
 		
-		j wroc
+		j back
 		
-	petlaResetPlansza_link:
-		bgt $t0, 10, wroc
+	resetBoardLoop_link:
+		bgt $t0, 10, back
 		addi $t2, $t0, 48
 		sb $t2, 0($t1)
 		addi $t1, $t1, 1
 		addi $t0, $t0, 1
-		j petlaResetPlansza_link
+		j resetBoardLoop_link
 	
 	
 	# Organizacja gry
-	liczbaRund:
+	number_of_rounds:
 		li $v0, 4
     		la $a0, prompt1
     		syscall
@@ -131,14 +133,14 @@
     		syscall
     		move $s0, $v0
     		bgt $s0, 0, mainGameLoop
-    		jal blednyInput_link
-    		j liczbaRund
+    		jal badInput_link
+    		j number_of_rounds
     		
 	mainGameLoop:
-    		bge $s3, $s0, wyniki
+    		bge $s3, $s0, results
     		addi $s3, $s3, 1
     		
-    		jal resetPlansza_link
+    		jal resetBoard_link
     		
     		li $v0, 4
     		la $a0, round
@@ -152,13 +154,13 @@
     		
     		move $t3, $zero
     		li $t4, 1
-    		jal rundaGracz_link
+    		jal humanTurn_link
     		
     		j mainGameLoop
-    	punktX:
+    	pointX:
     		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal wyswietlPlansze_link
+		jal showBoard_link
     		addi $s1, $s1, 1
     		
     		li $v0, 4
@@ -170,15 +172,15 @@
     		lb $a0, newLine
     		syscall
     		
-		jal aktualnyWynik_link
+		jal currentScore_link
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
     		
-    		j wroc
+    		j back
     	punktO:
     		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal wyswietlPlansze_link
+		jal showBoard_link
     		addi $s2, $s2, 1
     		
     		li $v0, 4
@@ -190,15 +192,15 @@
     		lb $a0, newLine
     		syscall
     		
-		jal aktualnyWynik_link
+		jal currentScore_link
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
     		
-    		j wroc
-    	remis:
+    		j back
+    	tie:
     		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal wyswietlPlansze_link
+		jal showBoard_link
     		#addi $s1, $s1, 1
     		#addi $s2, $s2, 1
     		
@@ -209,13 +211,13 @@
     		lb $a0, newLine
     		syscall
     		
-		jal aktualnyWynik_link
+		jal currentScore_link
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
     		
-    		j wroc
+    		j back
     		
-    	aktualnyWynik_link:
+    	currentScore_link:
     		li $v0, 4
     		la $a0, prompt5
     		syscall
@@ -246,21 +248,23 @@
     		li $v0, 11
     		lb $a0, newLine
     		syscall
-    		j wroc
-    	wyniki:
+    		j back
+
+    	results:
     		sub $s3, $s1, $s2
-    		beqz $s3, wynikRemis
+    		beqz $s3, resultTie
     		li $v0, 4
     		la $a0, prompt6
     		syscall
     		li $v0, 11
-    		bgezal $s3, ustawX
-    		bltzal $s3, ustawO
+    		bgezal $s3, setX
+    		bltzal $s3, setO
     		syscall
     		lb $a0, newLine
     		syscall
     		j exit
-    	wynikRemis:
+
+    	resultTie:
     		li $v0, 4
     		la $a0, prompt10
     		syscall
@@ -268,8 +272,9 @@
     		lb $a0, newLine
     		syscall
     		j exit
-	sprawdzStanPlanszy_link:
-		la $t5, plansza
+
+	checkBoardState_link:
+		la $t5, board
 		
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
@@ -278,61 +283,65 @@
 		lb $t0,0($t5)
 		lb $t1,1($t5)
 		lb $t2,2($t5)
-		jal sprawdzLinie_link
+		jal checkLine_link
 		# Rzad 2
 		lb $t0,3($t5)
 		lb $t1,4($t5)
 		lb $t2,5($t5)
-		jal sprawdzLinie_link
+		jal checkLine_link
 		# Rzad 3
 		lb $t0,6($t5)
 		lb $t1,7($t5)
 		lb $t2,8($t5)
-		jal sprawdzLinie_link
+		jal checkLine_link
 		# Kolumna 1
 		lb $t0,0($t5)
 		lb $t1,3($t5)
 		lb $t2,6($t5)
-		jal sprawdzLinie_link
+		jal checkLine_link
 		# Kolumna 2
 		lb $t0,1($t5)
 		lb $t1,4($t5)
 		lb $t2,7($t5)
-		jal sprawdzLinie_link
+		jal checkLine_link
 		# Kolumna 3
 		lb $t0,2($t5)
 		lb $t1,5($t5)
 		lb $t2,8($t5)
-		jal sprawdzLinie_link
+		jal checkLine_link
 		# Przekatna 1
 		lb $t0,0($t5)
 		lb $t1,4($t5)
 		lb $t2,8($t5)
-		jal sprawdzLinie_link
+		jal checkLine_link
 		# Przekatna 2
 		lb $t0,2($t5)
 		lb $t1,4($t5)
 		lb $t2,6($t5)
-		jal sprawdzLinie_link
-		# Remis
+		jal checkLine_link
+		# Tie
 		move $t0, $zero
 		move $t1, $zero
-		jal sprawdzRemis_link
+		jal checkTie_link
 		
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	sprawdzLinie_link:
-		beq $t0, $t1, sprawdzDrugie
-		j wroc
-	sprawdzDrugie:
-		beq $t0, $t2, sprawdzZnak
-		j wroc
-	sprawdzZnak:
+		j back
+
+	checkLine_link:
+		beq $t0, $t1, checkSecond
+		j back
+
+	checkSecond:
+		beq $t0, $t2, checkCharacter
+		j back
+
+	checkCharacter:
 		beq $t0, 'X', pktX
 		beq $t0, 'O', pktO
-		j wroc
-	podliczLinie:
+		j back
+
+	countLines:
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
 		
@@ -340,68 +349,78 @@
 		move $t8, $zero
 		move $t9, $zero
 		move $s4, $t0
-		jal zliczaj
+		jal counting
 		move $s4, $t1
-		jal zliczaj
+		jal counting
 		move $s4, $t2
-		jal zliczaj
+		jal counting
 		
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	zliczaj:
-		beq $s4, 'X', dodajX
-		beq $s4, 'O', dodajO
-		j dodajWolne
-	dodajX:
+		j back
+
+	counting:
+		beq $s4, 'X', addX
+		beq $s4, 'O', addO
+		j addFree
+
+	addX:
 		addi $t7, $t7, 1
-		j wroc
-	dodajO:
+		j back
+
+	addO:
 		addi $t8, $t8, 1
-		j wroc
-	dodajWolne:
+		j back
+
+	addFree:
 		addi $t9, $t9, 1
-		j wroc
+		j back
+
 	pktX:
 		li $t3, 1
-		j wroc
+		j back
+
 	pktO:
 		li $t3, 2
-		j wroc
-	ustawRemis:
+		j back
+
+	setTie:
 		li $t3, 3
-		j wroc
-	sprawdzRemis_link:
-		beq $t0, 9, ustawRemis
-		beq $t1, 9, wroc
+		j back
+
+	checkTie_link:
+		beq $t0, 9, setTie
+		beq $t1, 9, back
 		lb $t2, 0($t5)
 		addi $t5, $t5, 1
 		addi $t1, $t1, 1
-		beq $t2, 'X', dolicz
-		beq $t2, 'O', dolicz
-		j sprawdzRemis_link
-	dolicz:
+		beq $t2, 'X', addcount
+		beq $t2, 'O', addcount
+		j checkTie_link
+
+	addcount:
 		addi $t0, $t0, 1
-		j sprawdzRemis_link
+		j checkTie_link
     	# Gra
-    	rundaGracz_link:
-    		beq $t3, 1, punktX
+    	humanTurn_link:
+    		beq $t3, 1, pointX
     		beq $t3, 2, punktO
-    		beq $t3, 3, remis
+    		beq $t3, 3, tie
     		
     		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-    		jal wyswietlPlansze_link
+    		jal showBoard_link
 		
-		bgezal $t4, TG_ruchGracza
-    		bltzal $t4, TG_ruchKomputera
+		bgezal $t4, TG_humanTurn
+    		bltzal $t4, TG_computerTurn
     		mul $t4, $t4, -1
     		
-    		jal sprawdzStanPlanszy_link
+    		jal checkBoardState_link
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-    		j rundaGracz_link
-	TG_ruchGracza:
+    		j humanTurn_link
+
+	TG_humanTurn:
 		li $v0, 4
 		la $a0, prompt3
 		syscall
@@ -413,15 +432,16 @@
 		subi $t6, $t6, 1
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal zaznaczPole
+		jal selectField
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	TG_ruchKomputera:
+		j back
+
+	TG_computerTurn:
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		jal wyznaczOptymalnePole
-		jal zaznaczPole
+		jal selectOptimizedField
+		jal selectField
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
 		li $v0, 4
@@ -430,22 +450,24 @@
 		li $v0, 11
 		lb $a0, newLine
 		syscall
-		j wroc
-	zaznaczPole:
-		bltz $t6, blednyRuch
-		bge $t6, 9, blednyRuch
-		lb $t7, plansza($t6)
-		beq $t7, 'X', blednyRuch
-		beq $t7, 'O', blednyRuch
+		j back
+
+	selectField:
+		bltz $t6, invaidMove
+		bge $t6, 9, invaidMove
+		lb $t7, board($t6)
+		beq $t7, 'X', invaidMove
+		beq $t7, 'O', invaidMove
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
-		bgezal  $t4, ustawX
-		bltzal  $t4, ustawO
-		sb $a0, plansza($t6)
+		bgezal  $t4, setX
+		bltzal  $t4, setO
+		sb $a0, board($t6)
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	blednyRuch:
+		j back
+
+	invaidMove:
 		li $v0, 4
 		la $a0, prompt8
 		syscall
@@ -453,28 +475,32 @@
 		lb $a0, newLine
 		syscall
 		
-		j rundaGracz_link
-	ustawX:
+		j humanTurn_link
+
+	setX:
 		lb $a0, X
-		j wroc
-	ustawO:
+		j back
+
+	setO:
 		lb $a0, O
-		j wroc
-	wyznaczOptymalnePole:
+		j back
+
+	selectOptimizedField:
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
 		subi $t6, $zero, 1
 		
-		bltzal $t6, sprobujWygrac
-		bltzal $t6, sprobujZablokowac
-		bltzal $t6, sprobujZajacSrodek
-		bltzal $t6, sprobujZajacRogi
-		bltzal $t6, zajmijLosoweMiejsce
+		bltzal $t6, tryWin
+		bltzal $t6, tryBlock
+		bltzal $t6, tryCenter
+		bltzal $t6, tryEdge
+		bltzal $t6, tryRandom
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	sprobujWygrac:
-		la $t5, plansza
+		j back
+
+	tryWin:
+		la $t5, board
 		
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
@@ -483,94 +509,103 @@
 		lb $t0,0($t5)
 		lb $t1,1($t5)
 		lb $t2,2($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		# Rzad 2
 		lb $t0,3($t5)
 		lb $t1,4($t5)
 		lb $t2,5($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		# Rzad 3
 		lb $t0,6($t5)
 		lb $t1,7($t5)
 		lb $t2,8($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		# Kolumna 1
 		lb $t0,0($t5)
 		lb $t1,3($t5)
 		lb $t2,6($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		# Kolumna 2
 		lb $t0,1($t5)
 		lb $t1,4($t5)
 		lb $t2,7($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		# Kolumna 3
 		lb $t0,2($t5)
 		lb $t1,5($t5)
 		lb $t2,8($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		# Przekatna 1
 		lb $t0,0($t5)
 		lb $t1,4($t5)
 		lb $t2,8($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		# Przekatna 2
 		lb $t0,2($t5)
 		lb $t1,4($t5)
 		lb $t2,6($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszWygrac
+		jal countLines
+		jal checkIfCanWin
 		
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	sprawdzCzyMozeszWygrac:
+		j back
+
+	checkIfCanWin:
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
 		move $s4, $zero
-		bgezal $t4, sprawdzCzyDwaX
-		bltzal $t4, sprawdzCzyDwaO
-		jal sprawdzCzyJednoWolne
-		beq $s4, 2, toTutaj
+		bgezal $t4, checkIfTwoX
+		bltzal $t4, checkIdTwoO
+		jal checkIdOneFree
+		beq $s4, 2, itsHere
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	znalezionoPole:
+		j back
+
+	fieldFound:
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	sprawdzCzyDwaX:
+		j back
+
+	checkIfTwoX:
 		beq $t7, 2, incrementS4
-		j wroc
-	sprawdzCzyDwaO:
+		j back
+
+	checkIdTwoO:
 		beq $t8, 2, incrementS4
-		j wroc
-	sprawdzCzyJednoWolne:
+		j back
+
+	checkIdOneFree:
 		beq $t9, 1, incrementS4
-		j wroc
-	toTutaj:
+		j back
+
+	itsHere:
 		move $s4, $t0
 		blt $s4, 'A', setT6toS4
 		move $s4, $t1
 		blt $s4, 'A', setT6toS4
 		move $s4, $t2
 		j setT6toS4
+
 	incrementS4:
 		addi $s4, $s4, 1
-		j wroc
+		j back
+
 	setT6toS4:
 		subi $t6, $s4, '0'
 		subi $t6, $t6, 1
-		j wroc
-	sprobujZablokowac:
-		la $t5, plansza
+		j back
+
+	tryBlock:
+		la $t5, board
 		
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
@@ -579,88 +614,93 @@
 		lb $t0,0($t5)
 		lb $t1,1($t5)
 		lb $t2,2($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		# Rzad 2
 		lb $t0,3($t5)
 		lb $t1,4($t5)
 		lb $t2,5($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		# Rzad 3
 		lb $t0,6($t5)
 		lb $t1,7($t5)
 		lb $t2,8($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		# Kolumna 1
 		lb $t0,0($t5)
 		lb $t1,3($t5)
 		lb $t2,6($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		# Kolumna 2
 		lb $t0,1($t5)
 		lb $t1,4($t5)
 		lb $t2,7($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		# Kolumna 3
 		lb $t0,2($t5)
 		lb $t1,5($t5)
 		lb $t2,8($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		# Przekatna 1
 		lb $t0,0($t5)
 		lb $t1,4($t5)
 		lb $t2,8($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		# Przekatna 2
 		lb $t0,2($t5)
 		lb $t1,4($t5)
 		lb $t2,6($t5)
-		jal podliczLinie
-		jal sprawdzCzyMozeszZablokowac
+		jal countLines
+		jal checkIfYouCanBlock
 		
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	sprawdzCzyMozeszZablokowac:
+		j back
+
+	checkIfYouCanBlock:
 		subi $sp, $sp, 4
 		sw $ra, 0($sp)
 		move $s4, $zero
-		bgezal $t4, sprawdzCzyDwaO
-		bltzal $t4, sprawdzCzyDwaX
-		jal sprawdzCzyJednoWolne
-		beq $s4, 2, toTutaj
+		bgezal $t4, checkIdTwoO
+		bltzal $t4, checkIfTwoX
+		jal checkIdOneFree
+		beq $s4, 2, itsHere
 		lw $ra, 0($sp)
 		addi $sp, $sp, 4
-		j wroc
-	sprobujZajacSrodek:
+		j back
+
+	tryCenter:
 		li $s5, 4
-		lb $s4, plansza($s5)
+		lb $s4, board($s5)
 		blt $s4, 'A', setT6toS4
-		j wroc
-	sprobujZajacRogi:
+		j back
+
+	tryEdge:
 		li $s5, 0
-		lb $s4, plansza($s5)
+		lb $s4, board($s5)
 		blt $s4, 'A', setT6toS4
 		li $s5, 2
-		lb $s4, plansza($s5)
+		lb $s4, board($s5)
 		blt $s4, 'A', setT6toS4
 		li $s5, 6
-		lb $s4, plansza($s5)
+		lb $s4, board($s5)
 		blt $s4, 'A', setT6toS4
 		li $s5, 8
-		lb $s4, plansza($s5)
+		lb $s4, board($s5)
 		blt $s4, 'A', setT6toS4
-		j wroc
-	zajmijLosoweMiejsce:
-		la $t5, plansza
-	petlaZajmijLosoweMiejsce:
+		j back
+
+	tryRandom:
+		la $t5, board
+
+	pickRandomLoop:
 		lb $s4, 0($t5)
 		blt $s4, 'A', setT6toS4
 		addi $t5, $t5, 1
-		j petlaZajmijLosoweMiejsce
+		j pickRandomLoop
