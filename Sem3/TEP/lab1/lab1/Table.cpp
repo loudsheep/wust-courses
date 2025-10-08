e56d5e994f71743aa77c5f63a8cd394e5a09c539
loudsheep <loudsheep05@gmail.com>
2025-10-08 14:33:06 +0200

    tep: create Table class .h and .cpp files

--- Sem3/TEP/lab1/lab1/Table.cpp
+++ Sem3/TEP/lab1/lab1/Table.cpp
@@ -1,6 +1,32 @@
 #include "Table.h"
 #include <format>
 
+Table::Table()
+{
+}
+
 Table::Table(string name, int tableLen)
+{
+}
+
+Table::Table(Table& other)
+{
+}
+
+void Table::setName(string name)
+{
+}
+
+bool Table::setNewSize(int tableLen)
+{
+	return false;
+}
+
+Table* Table::clone()
+{
+	return nullptr;
+}
+
+Table::~Table()
 {
 }
--- Sem3/TEP/lab1/lab1/Table.h
+++ Sem3/TEP/lab1/lab1/Table.h
@@ -1,15 +1,29 @@
 #pragma once
+
 #include <string>
+
+using namespace std;
 
 class Table
 {
+private:
+	string name;
+	int* table;
+	int tableLen;
+
+	static const int DEFAULT_TABLE_LEN;
+	static const char* DEFAULT_NAME;
+
 public:
 	Table();
 	Table(string name, int tableLen);
 	Table(Table& other);
 
-private:
-	string name;
-	int tableLen;
+	void setName(string name);
+	bool setNewSize(int tableLen);
+
+	Table* clone();
+
+	~Table();
 };
 
