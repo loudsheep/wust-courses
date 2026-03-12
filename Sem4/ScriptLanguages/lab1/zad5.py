e46c189c341bcbd71ac84aac18fd6b208cb94269
loudsheep <loudsheep05@gmail.com>
2026-03-12 08:47:51 +0100

    script: lab1

--- Sem4/ScriptLanguages/lab1/zad5.py
+++ Sem4/ScriptLanguages/lab1/zad5.py
@@ -1,3 +1,12 @@
 import pyfiglet
 
-pyfiglet.print_figlet("Języki skryptowe")
+# pyfiglet.print_figlet("Języki skryptowe")
+
+text = input("Podaj tekst do wygenerowania: ")
+font = input("Podaj czcionkę (np. slant, standard, big): ")
+
+try:
+    ascii_art = pyfiglet.figlet_format(text, font=font)
+    print(ascii_art)
+except pyfiglet.FontNotFound:
+    print(f"Czcionka '{font}' nie została znaleziona. Użyj jednej z dostępnych czcionek.")
